6922e09843c447f5338721ab597507d26eb0cebf
UsernameN0tAvailable <mattia.pedrazzi@outlook.com>
2022-04-01 02:21:41 +0200

    skip lambda functions

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 
@@ -14,6 +15,9 @@ const (
 
 	slash = byte('/')
 	star = byte('*')
+	str = byte('"')
+	newLine = byte('\n') // only works on unix systems
+	at = byte('@') 
 )
 
 var tot int = 0
@@ -51,20 +55,19 @@ func listDirs(root string) {
 		if ext := filepath.Ext(file.Name()); !file.IsDir() && ext == ".java" {
 			parseJavaFile(root + string(os.PathSeparator) + file.Name())
 		} else if file.IsDir() {
-			//fmt.Println("DIR: ", file.Name())
 			listDirs(root + string(os.PathSeparator) + file.Name())
 		}
 	}
 }
 
 func parseJavaFile(filePath string) {
-	fmt.Println("PARSE FILE: " + filePath)
+	//fmt.Println("PARSE FILE: " + filePath)
 	tot += 1
 
 	content, err := os.ReadFile(filePath)
 
 	if err != nil {
-		fmt.Println("Couldnt read file at: " + filePath)
+//		fmt.Println("Couldnt read file at: " + filePath)
 		return
 	}
 
@@ -72,23 +75,27 @@ func parseJavaFile(filePath string) {
 
 //	fmt.Println(string(content))
 
-	os.Exit(3)
+//	os.Exit(3)
 }
 
 func parseFile(content []byte) {
 
-//	inScope := false
 	inComment := false
 	inDocumentation := false
+	inLogic := false
+	inString := false
 
 	documentations := make([]string, 0)
 	comments := make([]string, 0)
 
 	start := 0
 
+	scopeCount := 0
+
+
 	for i, c := range content {
 
-		if c == slash {
+		if c == slash && !inLogic {
 			nextIndex := i + 1
 			prevIndex := i - 1
 			if !inComment && nextIndex < len(content) && star == content[nextIndex] {
@@ -103,16 +110,102 @@ func parseFile(content []byte) {
 					inDocumentation = false
 				} else if inComment {
 					comments = append(comments, string(content[start:nextIndex]))
-		//			fmt.Println(comments[0], start, i)
 				}
 				inComment = false
 			}
+		} else if c == scopeOn && !inComment && !inString {
+			inLogic = true
+
+			var signature string
+
+			// probably a class
+			if scopeCount == 0 {
+				signature = string(findFirstSignature(i, content))
+			} else {
+				signature = string(findFirstSignature(i, content))
+			}
+
+			if isValidSignature(signature) {
+				fmt.Println(signature)
+			}
+
+			scopeCount++
+
+		} else if c == scopeOff && !inComment && !inString {
+			scopeCount--
+
+			if scopeCount == 0 {
+				inLogic = false
+			}
+		} else if c == str {
+			inString = !inString
+		}
+	}
+}
+
+
+func isValidSignature(s string) bool {
+
+	trimmed := strings.TrimSpace(s)
+
+	if len(trimmed) == 0 {
+		return false
+	} else if trimmed[0] == at {
+		return false
+	} else {
+		fields := strings.Fields(trimmed)
+		predicate := fields[0]
+		if len(predicate) == 0 {
+			return false
+		}
+
+		for _,field := range fields {
+			subfields := strings.Fields(field)
+
+			for _,f := range subfields {
+				if !isValidSignatureKeyWord(f) {
+					return false
+				}
+			}
+		}
+		return true
+	}
+}
+
+func isValidSignatureKeyWord(predicate string) bool {
+	return predicate != "for" && predicate != "if" && predicate != "while" && predicate != "else" && predicate != "try" && predicate != "catch" && predicate != "finally" && predicate != "->"
+}
+
+
+func findFirstSignature(i int, content []byte) []byte {
+
+	end := i
+
+	for true {
+		if content[i] == newLine {
+			return content[i:end]
+		} else if i >= 1 {
+			i--
 		}
 	}
 
+	return nil
+}
 
-	fmt.Println(comments)
 
+func findSignature(i int, content []byte) []byte {
+
+	end := i
+
+	for true {
+		if content[i] == scopeOff || content[i] == slash {
+			return content[i:end]
+		} else if i >= 1 {
+			i--
+		}
+	}
+
+	return nil
 }
 
 
