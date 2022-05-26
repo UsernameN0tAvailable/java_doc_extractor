1764a54195fb2df6c32491cafdbbd33a7bda76f5
UsernameN0tAvailable <mattia.pedrazzi@outlook.com>
2022-05-26 18:12:18 +0200

    return type and throws

--- method.go
+++ method.go
@@ -4,12 +4,16 @@ import (
 	"strings"
 )
 
+var visibility = []string {"public", "private", "protected"}
+
 type Method struct {
 	Signature string `json:"signature"`
 	Documentation string `json:"documentation"`
 	signatureStart int
 	Line int `json:"line"`
 	Body string  `json:"code"`
+	ReturnType string
+	Throws bool
 }
 
 func (m *Method) AddBody (body string, currentIndex int) {
@@ -49,8 +53,49 @@ func (m*Method) GetName() string {
 
 	return sp[len(sp) - 1]
 }
-
  
 func NewMethod(s string, d string, signatureStart int, signatureLineStart int) Method {
-	return Method{Signature: s, Documentation: d, signatureStart: signatureStart, Line: signatureLineStart}
+
+	signatureFields := strings.Fields(s)
+
+
+	return Method{
+		Signature: s, 
+		Documentation: d, 
+		signatureStart: 
+		signatureStart, 
+		Line: signatureLineStart, 
+		Throws: sliceContains(signatureFields, "throws"), 
+		ReturnType: findReturnValue(signatureFields) }
 }
+
+
+func findReturnValue(signature []string) string {
+	for i, e := range signature {
+		if sliceContains(visibility, e) && len(signature) > i + 1 {
+			if signature[i + 1] == "abstract" && len(signature) > i + 2 {
+				return signature[i + 2]
+			} else {
+				return signature[i+1]
+		}
+		}
+	}
+
+	if signature[0] == "abstract" && len(signature) > 1 {
+		return signature[1]
+	}
+
+	return signature[0]
+}
+
+func sliceContains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+
